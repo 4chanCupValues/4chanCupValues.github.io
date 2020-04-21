4468d0a3ef8987738b9e60939423c48d43ee12d0
Xyl <xyl@xyllon.com>
2020-04-22 03:21:34 +1000

    Initial commit

---
+++ questions.js
@@ -0,0 +1,39 @@
+questions = 
+    [[//Wet-Dry
+        [//Wet
+            "I'm excited to check my underwear after farting.",
+            "A fart that leaves no mark as its legacy is worthless.",
+            "I enjoy the warmth of fart liquid.",
+			"All of my underwear has brown stains."
+        ],
+        [//Dry
+            "Wet farts are just poop.",
+            "Cleaning up a wet fart is more trouble than it's worth.",
+            "I always change after a wet fart.",
+			"I often go to the toilet to fart in case it's more than just a fart."
+        ]],[//Smelly-Odorless
+        [//Smelly
+            "The more people who smell a fart, the better.",
+            "A fart that doesn't smell may as well not be a fart at all.",
+            "I like the lingering odor of a fart.",
+            "I often Dutch oven myself."
+        ],
+        [//Odorless
+            "A fart's smell should be known only to the farter.",
+            "The smell of my own farts disgust me.",
+            "The smell of other people's farts disgust me.",
+            "If I wanted to smell shit, I'd go to the bathroom."
+        ]],[//Quiet-Loud
+        [//Quiet
+            "Noisy farts ruin the surprise.",
+            "It's embarrassing when people hear me fart.",
+            "I negatively judge those who fart loudly in public.",
+            "I prefer slowly, quietly allowing air to escape."
+        ],
+        [//Loud
+            "I like being able to feel the breeze from a fart.",
+            "I like everyone to know it was me who farted.",
+            "The noise is half of the fun.",
+            "I angle myself to make the noisiest fart possible."
+        ]
+    ]];
