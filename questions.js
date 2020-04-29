0071722d8f7945fc6d9868d541fb9104137ad8c8
4chanCupValues <64515203+4chanCupValues@users.noreply.github.com>
2020-04-29 15:30:32 +1000

    Update questions.js

--- questions.js
+++ questions.js
@@ -7,6 +7,8 @@ questions =
 	    "The more anal the manager, the more the team fails."
 	    "There's no danger in making the goalkeeper take a penalty."
 	    "All my team's problems can be fixed with gegenpress."
+	    "Fluid formations work."
+	    "Experimenting with unorthodox formations (2-3-5, 4-5-1) pays off."
         ],
         [//Serious
             "A team cannot win a cup without proper testing.",
@@ -15,6 +17,8 @@ questions =
 	    "Medals further back than a CMF don't work."
 	    "Going +2 is a risk that fails more than it succeeds."
 	    "Choosing the right player cards is key to a successful team."
+	    "I use all my substitutions to sub on players with the best conditions."
+	    "My team is set up in the export and is all ready to go."
         ]],[//Manager-Merc
         [//Manager
             "Engaging with the board builds better rosters.",
@@ -23,6 +27,8 @@ questions =
             "The essence of a team is proportional to the quality of their in-game models."
 	    "I would use a Discord to communicate with my board."
 	    "All polls should require a verification question."
+	    "Each team could benefit from a full-body model or two."
+	    "I keep the team's wiki page updated."
         ],
         [//Merc
             "Polls don't need to be run before every cup my team is participating in.",
@@ -31,6 +37,8 @@ questions =
             "Player-specific goalhorns are not necessary."
 	    "As long as I get POI, I'm still a manager."
 	    "I don't need a co-manager for any reason."
+	    "An anthem doesn't need to have hype, it just needs to be related to the team."
+	    "Three outfielder kits is all you need."
         ]],[//Shitposter-Nofun
         [//Shitposter
             "Player lore interests me.",
@@ -39,6 +47,8 @@ questions =
             "It's better to make a name for myself in the cup rather than be a nobody who manages."
 	    "I always spam emotes in chat when someone scores, even if it isn't my team."
 	    "People who take the cup seriously are retarded."
+	    "The cup's IRC and Discord need spicing up."
+	    "Encouraging new teams in the cup is great, even if the boards themselves are boring."
         ],
         [//Nofun
             "Copypasta spam is annoying.",
@@ -47,5 +57,7 @@ questions =
             "/4ccg/ should be reserved for serious discussion."
 	    "Color commentary makes the experience worse."
 	    "Jumping on /4ccg/ shitpost bandwagons (e.g. >page 9, deer ses, LMAOposting) ruins threads."
+	    "Even though this is 4chan, the cup shouldn't be outright anarchical."
+	    "All cup discussion should remain limited to /vg/, /sp/ and /mlp/."
         ]
     ]];
