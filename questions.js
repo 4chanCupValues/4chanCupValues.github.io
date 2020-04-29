06f9bf8e788449be0f13aced1dca87253fa82f4a
4chanCupValues <64515203+4chanCupValues@users.noreply.github.com>
2020-04-29 15:19:19 +1000

    Update questions.js

--- questions.js
+++ questions.js
@@ -4,36 +4,48 @@ questions =
             "It's worth putting meme formations in the first tactical export.",
             "Getting creative with sliders is worthwhile.",
             "Destroyer cards are a must.",
-			"The more anal the manager, the more the team fails."
+	    "The more anal the manager, the more the team fails."
+	    "There's no danger in making the goalkeeper take a penalty."
+	    "All my team's problems can be fixed with gegenpress."
         ],
         [//Serious
             "A team cannot win a cup without proper testing.",
             "There is no need to throw seven niggas forward in desperate times.",
             "I only use my timeouts if my tactics aren't working.",
-			"Medals further back than a CMF don't work."
+	    "Medals further back than a CMF don't work."
+	    "Going +2 is a risk that fails more than it succeeds."
+	    "Choosing the right player cards is key to a successful team."
         ]],[//Manager-Merc
         [//Manager
             "Engaging with the board builds better rosters.",
             "Gold players should change with the board's wishes.",
             "There is no excuse for having in-game faces on the team.",
             "The essence of a team is proportional to the quality of their in-game models."
+	    "I would use a Discord to communicate with my board."
+	    "All polls should require a verification question."
         ],
         [//Merc
             "Polls don't need to be run before every cup my team is participating in.",
             "Chants aren't really that important.",
             "There's nothing wrong with managing a board I don't use.",
             "Player-specific goalhorns are not necessary."
+	    "As long as I get POI, I'm still a manager."
+	    "I don't need a co-manager for any reason."
         ]],[//Shitposter-Nofun
         [//Shitposter
             "Player lore interests me.",
             "It's okay to shitpost on /4ccg/ when the thread is dead.",
             "I use my tripcode on /4ccg/ outside of verification.",
             "It's better to make a name for myself in the cup rather than be a nobody who manages."
+	    "I always spam emotes in chat when someone scores, even if it isn't my team."
+	    "People who take the cup seriously are retarded."
         ],
         [//Nofun
             "Copypasta spam is annoying.",
             "I aspire to be part of /admin/ one day, or even the new commissioner.",
             "Playing chicken with deadlines is a meaningless endeavor.",
             "/4ccg/ should be reserved for serious discussion."
+	    "Color commentary makes the experience worse."
+	    "Jumping on /4ccg/ shitpost bandwagons (e.g. >page 9, deer ses, LMAOposting) ruins threads."
         ]
     ]];
